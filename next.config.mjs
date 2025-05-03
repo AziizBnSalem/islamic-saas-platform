45b16802669b3396e7a824e2bc6ef320c9eb673a
AziizBnSalem <mohamedaziz.bensalem@gmail.com>
2025-05-03 06:45:55 +0100

    dev pwa unable

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,6 @@ const pwaConfig = withPWA({
   dest: 'public', // Where service worker and assets will be generated
   register: true, // Automatically register the service worker
   skipWaiting: true, // Skip waiting for service worker activation
-  disable: process.env.NODE_ENV === 'development', // Optional: Disable PWA in development
 });
 
 const nextConfig = {
